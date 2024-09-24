2af5fd1c94f5e93e4d1e0a01d13753afcf41fb51
Dat Trinh <dat.trinh@evizi.com>
2024-09-24 16:41:18 +0700

    Remove ignored files and apply .gitignore

--- DataAccessLayer/obj/Debug/net8.0/DataAccessLayer.AssemblyInfo.cs
+++ DataAccessLayer/obj/Debug/net8.0/DataAccessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcb28b6f95fd33e34090e770e0cc56479941262c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cbd368fe0541680d764204fa6325571f9d3095b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
